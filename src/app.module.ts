7a2cbacbc7eecb52f7eb4d7276b7018130cc30ec
miri <github@miritech.com>
2022-05-23 11:32:38 +0530

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
+})
+export class AppModule {}
